8d53692b7d6bd75432c43ad0a3603e29adaa5b24
Oliver Spring <oliver9589@gmail.com>
2017-09-19 03:40:21 +0000

    automatically backed up by learn

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 class PoemWriter extends React.Component {
 
-  const isValidPoem(poem) {
+  isValidPoem(poem) {
     const poemLines = poem.split('\n').filter(l => l);
     const isRightAmountOfLines = poemLines.length === 3;
     const hasRightAmountOfWords = countWords(poemLines[0]) === 5 && countWords(poemLines[1]) === 3 && countWords(poemLines[2]) === 5;
