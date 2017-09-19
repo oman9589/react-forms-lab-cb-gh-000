1c58ff5aa0898de0f0d2db65a9ad8a92083e9dfc
Oliver Spring <oliver9589@gmail.com>
2017-09-19 03:09:49 +0000

    automatically backed up by learn

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -13,7 +13,7 @@ class PoemWriter extends React.Component {
   evaluatePoem = () => {
     const lines = () => { return this.split(/\r*\n/); }
     const lineCount = () => { return this.lines().length; }
-    if (this.lines().length == 3) {
+    if (this.linesCount === 3) {
       this.setState({
         ...this.state,
         isValid: true
